5e9bfaae4846a2aebd21b719cb5375a8e17e4a4e
andrewstech <andrew@win11react.com>
2023-04-29 20:44:35 +0000

    first post

---
+++ docs/Server/Intro.md
@@ -0,0 +1,7 @@
+---
+sidebar_position: 1
+---
+
+# Getting Started with the Server
+
+The server is powered by [fastapi](https://fastapi.tiangolo.com/).
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,6 +76,7 @@ const config = {
             position: 'left',
             label: 'Docs',
           },
+          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/peerbrain',
             label: 'GitHub',
